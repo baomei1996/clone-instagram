50095363c5c5a95f9025cf4db3e7a20459be52e1
baomei1996 <qhal4686@gmail.com>
2023-10-23 21:25:15 +0900

    Feat: Create the header and image sections of the post component

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HiDotsHorizontal } from "react-icons/hi";
 
 type PostProps = {
     username: string;
@@ -9,8 +10,19 @@ type PostProps = {
 
 export default function Post({ img, userImg, username, caption }: PostProps) {
     return (
-        <div>
-            <h1>{username}</h1>
+        <div className="bg-white my-7 border rounded-md">
+            {/* Post Header */}
+            <div className="flex items-center p-5">
+                <img
+                    className="h-12 rounded-full object-cover border p-1 mr-3"
+                    src={userImg}
+                    alt={username}
+                />
+                <p className="font-bold flex-1">{username}</p>
+                <HiDotsHorizontal />
+            </div>
+            {/* Post Image */}
+            <img className="object-cover w-full" src={img} alt="" />
         </div>
     );
 }
