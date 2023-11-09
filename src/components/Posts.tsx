c2f9d821f0a91b84b0396064c54cac02809c521f
baomei1996 <qhal4686@gmail.com>
2023-11-09 22:13:28 +0900

    Feat: Get the post data from database and show it in the feed section

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,41 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Post from "./Post";
+import {
+    QuerySnapshot,
+    collection,
+    onSnapshot,
+    orderBy,
+    query,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+
+type Post = {
+    id: string;
+    username: string;
+    profileImg: string;
+    image: string;
+    caption: string;
+};
 
 export default function Posts() {
-    const posts = [
-        {
-            id: "1",
-            username: "natasha",
-            userImg:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsynwv-5qtogtOwJbIjaPFJUmHpzhxgqIAug&usqp=CAU",
-            img: "https://images.unsplash.com/photo-1695097737988-26ef6731d56d?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHx8",
-            caption: "thanks for watching",
-        },
-        {
-            id: "2",
-            username: "david",
-            userImg:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsynwv-5qtogtOwJbIjaPFJUmHpzhxgqIAug&usqp=CAU",
-            img: "https://images.unsplash.com/photo-1695349092661-6e53e5182140?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw1fHx8ZW58MHx8fHx8",
-            caption: "Nice picture",
-        },
-        {
-            id: "3",
-            username: "natasha",
-            userImg:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsynwv-5qtogtOwJbIjaPFJUmHpzhxgqIAug&usqp=CAU",
-            img: "https://images.unsplash.com/photo-1697194097089-c78003fc69a3?auto=format&fit=crop&q=60&w=400&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D",
-            caption: "Night street",
-        },
-    ];
+    const [posts, setPosts] = useState<Array<Post>>([]);
+
+    useEffect(() => {
+        const unsubscribe = onSnapshot(
+            query(collection(db, "posts"), orderBy("timestamp", "desc")),
+            (snapshot) => {
+                const posts: Array<Post> = [];
+                snapshot.docs.map((doc) => {
+                    posts.push({
+                        id: doc.id,
+                        username: doc.data().username,
+                        profileImg: doc.data().profileImg,
+                        image: doc.data().image,
+                        caption: doc.data().caption,
+                    });
+                });
+                setPosts(posts);
+            }
+        );
+
+        return unsubscribe;
+    }, []);
+
     return (
         <div>
             {posts.map((post) => (
                 <Post
                     key={post.id}
                     username={post.username}
-                    userImg={post.userImg}
-                    img={post.img}
+                    userImg={post.profileImg}
+                    img={post.image}
                     caption={post.caption}
                 />
             ))}
