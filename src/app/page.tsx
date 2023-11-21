8500d4e4bd4787c0790797536afc40b30054b3fb
baomei1996 <qhal4686@gmail.com>
2023-11-22 00:13:19 +0900

    Feat: Metadata 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,16 @@ import Header from "@/components/Header";
 import Feed from "@/components/Feed";
 import UploadModal from "@/components/UploadModal";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "clone-instagram",
+    description: "Instagram clone built with Next.js and Tailwind CSS",
+    icons: {
+        icon: "/instagram_icon.png",
+    },
+};
+
 export default function Page() {
     return (
         <div className="bg-gray-50 min-h-screen">
