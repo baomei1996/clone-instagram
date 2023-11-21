99374334c4f0a0dd9e126699be985ba8a1463d47
baomei1996 <qhal4686@gmail.com>
2023-11-22 00:13:43 +0900

    Feat: Add Secret

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -10,6 +10,7 @@ const handler = NextAuth({
             clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
         }),
     ],
+    secret: process.env.SECRET,
     pages: {
         signIn: "/auth/signin",
     },
